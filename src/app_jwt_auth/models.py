7f4def5e94b1fd79ae23f17cde788e3eb4917de5
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-09 13:34:49 +0000

    model bugs fixed

--- src/app_jwt_auth/models.py
+++ src/app_jwt_auth/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
     name = models.TextField(_('Product Name'), max_length=25)
-    date = models.DateField(_("Date"), auto_now=True, auto_now_add=True, null=True, blank=True)
+    date = models.DateField(_("Date"), auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return 
