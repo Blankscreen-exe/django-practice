ce50671242644c13298a702c3e8541b38deb2491
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-09 13:35:55 +0000

    TDD initial

---
+++ src/app_tdd/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
+
+class AdminTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()# Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_admin_site(self):
+        # user opens web browser, navigates to admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+    
+    def test_admin_site(self):
+        # user opens web browser, navigates to admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+        # user clicks on the Users link
+        user_link = self.browser.find_elements_by_link_text('Users')
+        user_link[0].click()
+        # user verifies that user live@forever.com is present
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('live@forever.com', body.text)
+    
